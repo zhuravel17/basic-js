b27b60bfcd84c7f0370428aecd0e534368d37dcf
Zhuravel17 <kat.20030304@yandex.ru>
2024-12-24 00:06:54 +0400

    feat: solve repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,8 +15,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  const repeatTimes = Object.hasOwn(options, "repeatTimes") ? options.repeatTimes : 1;
+  const separator = Object.hasOwn(options, "separator") ? options.separator : '+';
+  let addition = '';
+  if (Object.hasOwn(options, "addition")) {
+    let add = options.addition;
+    if (typeof add === "string") {
+      addition = add;
+    } 
+    else {
+      addition = String(add);
+    }
+  }
+  const additionRepeatTimes = Object.hasOwn(options, "additionRepeatTimes") ? options.additionRepeatTimes : 1;
+  const additionSeparator = Object.hasOwn(options, "additionSeparator") ? options.additionSeparator : '|';
+
+  let addStr = ''
+  if (addition !== '') {
+    addStr = new Array(additionRepeatTimes)
+      .fill(addition)
+      .join(additionSeparator)
+  }
+  let res = new Array(repeatTimes)
+    .fill(str + addStr)
+    .join(separator);
+  return res; 
   // remove line with error and write your code here
 }
 
