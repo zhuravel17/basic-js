18e9fa129b8e787da952565c8381bb22464d0994
Zhuravel17 <kat.20030304@yandex.ru>
2024-12-24 20:25:49 +0400

    feat: solve VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(msg, key) {
+    if (!msg || !key) {
+      throw new Error("Incorrect arguments!")
+    }
+
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    const upper_msg = msg.toUpperCase();
+    const upper_key = key.toUpperCase();
+    let vig_key =  upper_key.repeat(Math.ceil(upper_msg.length / upper_key.length));
+    vig_key = vig_key.slice(0, upper_msg.length);
+    let key_index = 0;
+    let res = '';
+
+    for (let i = 0; i < upper_msg.length; i++) {
+      const char = upper_msg[i];
+      if (alphabet.includes(char)) {
+        const msg_index = alphabet.indexOf(char);
+        const key_char = vig_key[key_index];
+        const key_char_index = alphabet.indexOf(key_char);
+        const encrypted_char = alphabet[(msg_index + key_char_index) % 26];
+        res += encrypted_char;
+        key_index++;
+      }
+      else {
+        res += char;
+      }
+    }
+    return this.isDirect ? res : res.split('').reverse().join('')
+  }
+  decrypt(msg, key) {
+    if (!msg || !key) {
+      throw new Error("Incorrect arguments!")
+    }
+
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    const upper_msg = msg.toUpperCase();
+    const upper_key = key.toUpperCase();
+    let vig_key =  upper_key.repeat(Math.ceil(upper_msg.length / upper_key.length));
+    vig_key = vig_key.slice(0, upper_msg.length);
+    let key_index = 0;
+    let res = '';
+
+    for (let i = 0; i < upper_msg.length; i++) {
+      const char = upper_msg[i];
+      if (alphabet.includes(char)) {
+        const msg_index = alphabet.indexOf(char);
+        const key_char = vig_key[key_index];
+        const key_char_index = alphabet.indexOf(key_char);
+        const encrypted_char = alphabet[(msg_index - key_char_index + 26) % 26];
+        res += encrypted_char;
+        key_index++;
+      }
+      else {
+        res += char;
+      }
+    }
+    return this.isDirect ? res : res.split('').reverse().join('')
   }
 }
 
